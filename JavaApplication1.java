516743d5a75eef06d0749147e10e2cde34aadacd
PHALVES252 <62769652+PHALVES252@users.noreply.github.com>
2021-07-06 09:16:18 -0300

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author sala302b
+ */
+public class JavaApplication1 {
+
+   
+    /**
+     * @param args the command line arguments
+     */
+     
+
+     
+    public static void main(String[] args) {
+         //ESTE É UM PROGRAMA QUE VAI PRINTA NA TELA "helo world"
+         
+        System.out.println("Hello word");
+                 
+        //  int dataValue;
+        // dataValue = 100;
+         //int myInfo=200;
+        // int sum;
+        // int studentCount;
+        // int banckAccountBalance;
+        // int level2Training;
+         
+         
+         
+        // int myVar;
+       //  myVar=50;
+        //   System.out.println("myVar");
+        //   int anotherVar=100;
+        //  System.out.println("anotherVar");
+        //  myVar= anotherVar;
+        //   System.out.println("myVar");
+          
+           
+           
+        //   final int maxStudents= 25;
+       //  final int someVariable;
+       // int someOtherVariable= 100;
+        // someVariable= someOtherVariable;
+         
+         
+        // byte numberOfEnglishLetters= 26;
+      // short feetInAMile= 5280;
+        //int milesToSun=929600000;
+       // long milesInALightYear= 587900000000L;
+      // float kilometersInAMarathon=42.195f;
+     // double absoluteZeroInCelsius= 0.000000000ld;
+    // char regularU= 'U';
+ //  char  accentedU='\u00DA';
+ // boolean iLoveJava=true;
+   
+   
+   
+   // int firstValue= 100;
+  // int ontherValue= firstValue;
+ // firstValue= 50;
+  // ontherValue=700;
+  // int somerValue=5;
+  // System.out.prinln(++someValue);
+  // System.out.println(someValue);
+   
+   
+  // int someOtherValue=5;
+   //System.out.println(someOtherValue++);
+   
+  //int myValue= 50;
+  //myValue= 5;
+  //System.out.println(myValue);
+  
+    //int myOtherValue= 100;
+ //int val1=5;
+  //int val2=10;
+ //myOtherValue/=val1 * val2;
+ // System.out.println(myOtherValue);
+  
+         int intValueOne=50;
+         long longValueOne= intValueOne;
+         
+         long longValueTwo= 50;
+         int intValueTwo= (int) longValueTwo;
+}}
+    
